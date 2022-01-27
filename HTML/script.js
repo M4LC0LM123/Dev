ce6f982d8694ed7d0db8d828f97adfb03be30457
M4LC0LM123 <lukaagni.malovic@gmail.com>
2022-01-27 11:25:18 +0100

    idk

--- HTML/script.js
+++ HTML/script.js
@@ -1,9 +1,14 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 function jump(){
-    character.classList.add("animate");
+    if (character.classList != "animate"){
+        character.classList.add("animate");
+    }
     setTimeout(function(){
         character.classList.remove("animate");
     },500);
 }
 
+var checkDead = setInterval(function() {
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+}, 10);
