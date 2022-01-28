ebd157001699f820abca6a8d59dc6d343c5e3fa6
M4LC0LM123 <lukaagni.malovic@gmail.com>
2022-01-28 19:06:57 +0100

    app 2.0

---
+++ ChatApp2/server.py
@@ -0,0 +1,39 @@
+import socket
+from threading import Thread
+
+# server's IP address
+SERVER_HOST = input("IP: ")
+SERVER_PORT = 5050 # port we want to use
+separator_token = "<SEP>" # we will use this to separate the client name & message
+
+client_sockets = set()
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((SERVER_HOST, SERVER_PORT))
+s.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+
+def listen_for_client(cs):
+    while True:
+        try:
+            msg = cs.recv(1024).decode()
+        except Exception as e:
+            print(f"[!] Error: {e}")
+            client_sockets.remove(cs)
+        else:
+            msg = msg.replace(separator_token, ": ")
+        for client_socket in client_sockets:
+            client_socket.send(msg.encode())
+
+
+while True:
+    client_socket, client_address = s.accept()
+    print(f"[+] {client_address} connected.")
+    client_sockets.add(client_socket)
+    t = Thread(target=listen_for_client, args=(client_socket,))
+    t.daemon = True
+    t.start()
+
+for cs in client_sockets:
+    cs.close()
+s.close()
